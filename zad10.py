b73596dd520046b92ecaf1568ad1653d50413061
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-04-07 21:20:21 +0200

    Delete zad10.py

--- zad10.py
+++
@@ -1,5 +0,0 @@
-temp = float(input("Podaj temperature w stopniach Celsjusza: ").replace(",","."))
-
-r = temp * (9/5) + 32
-
-print(f"Podana temperatura w stopniach Celsjusza to tyle stopni Fahrenheita: {r}")
