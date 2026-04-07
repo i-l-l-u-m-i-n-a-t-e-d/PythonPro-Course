05ee5c82cd9ec5f0e67eddf90fe570b354c92912
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-04-07 21:31:02 +0200

    Add files via upload

---
+++ lesson1/zad1.py
@@ -0,0 +1,4 @@
+print("Mam na imie Maksymilian i jestem studentem.")
+print("Lubie jezdzic rowerem.")
+print("Ucze sie pythona bo chce tworzyc programy i na tym zarabiac.")
+
---
+++ lesson1/zad4.py
@@ -0,0 +1,1 @@
+print(f"Kod ASCII dla dużej litery to: {ord('M')} a dla małej litery to: {ord('m')}")
