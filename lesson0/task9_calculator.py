3a2d0f4328eb960bee7805f788cccdbfc11ac1e3
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-04-07 13:56:25 +0200

    Fix subtraction and division operations in calculator

--- lesson0/task9_calculator.py
+++ lesson0/task9_calculator.py
@@ -11,16 +11,18 @@ if z == '+':
     print(f"Wynik dodawania: {a+b}")
 
 elif z == '-':
-    print(f"Wynik odejmowania: {abs(a-b)}")
+    print(f"Wynik odejmowania: {a-b}")
 
 elif z == '*':
     print(f"Wynik mnozenia: {a*b}")
 
-else:
+elif z == '\':
     if b == 0:
         print("Nie mozna dzielic przez 0.")
     else:
         print(f"Wynik dzielenia: {a/b}")
+else:
+    print("Wpisales niepoprawny znak")
 
 
 
