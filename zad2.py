4620f724171b2d5a9e7d560811b5dd96a0f60e5c
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-04-07 21:20:29 +0200

    Delete zad2.py

--- zad2.py
+++
@@ -1,5 +0,0 @@
-imie = input("Podaj swoje imie: ")
-wiek = input("Ile masz lat? ")
-miasto = input("W jakim mieście mieszkasz? ")
-
-print(f"A więc, masz na imię {imie}, masz {wiek} lat i mieszkasz w mieście {miasto}.")
