217b4eec2d60fa246b6900c816ae6b9ae3d243b4
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-04-10 20:50:53 +0200

    Refactor rectangle perimeter calculation with error handling
    Added error flag to handle negative dimensions.

--- lesson1/zad3.py
+++ lesson1/zad3.py
@@ -1,12 +1,14 @@
 height = float(input("Podaj dlugosc prostokata: ".replace(",",".")))
 width = float(input("Podaj szerokosc prostokata: ".replace(",",".")))
 
+error = False
 if height < 0:
     print("Dlugosc nie moze byc ujemna.")
+    error = True
     
 elif width < 0: 
     print("Szerokosc nie moze byc ujemna.")
-
+    error = True
 else:
     
     circuit = 2*(height+width)
