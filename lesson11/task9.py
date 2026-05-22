2104385053354a0affe9e9f0324d6bc851629a4a
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-05-23 00:11:24 +0200

    Update task9.py

--- lesson11/task9.py
+++ lesson11/task9.py
@@ -7,29 +7,22 @@ class BrakSrodkowError(Exception):
 
 @dataclass
 class KontoBankowe:
-
     _saldo: float
 
     @property
     def saldo(self):
-
         return self._saldo
 
     def wplac(self, kwota):
-
         if kwota < 0:
-
             raise ValueError("Kwota wpłaty nie może być ujemna.")
         self._saldo += kwota
 
     def wyplac(self, kwota):
-
         if kwota < 0:
-
             raise ValueError("Kwota wypłaty nie może być ujemna.")
 
         if kwota > self._saldo:
-
             raise BrakSrodkowError("Brak środków na koncie.")
 
         self._saldo -= kwota
@@ -38,7 +31,6 @@ class KontoBankowe:
 konto = KontoBankowe(100)
 
 try:
-    
     konto.wplac(50)
     print(konto.saldo)
 
