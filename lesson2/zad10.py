f4b925014dcfc2b59c6015b064871e78dc553d55
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-06-05 15:21:35 +0200

    Update zad10.py

--- lesson2/zad10.py
+++ lesson2/zad10.py
@@ -1,3 +1,8 @@
-name_surname = input("Wpisz swoje imie i nazwisko (Imie Nazwisko): ").strip().title()
+name_surname = input("Wpisz swoje imię i nazwisko: ")
 
-print(name_surname, len(name_surname))
+clean_name_surname = name_surname.strip()
+formatted_name_surname = clean_name_surname.title()
+name_surname_length = len(formatted_name_surname)
+
+print(f"Sformatowane dane: {formatted_name_surname}")
+print(f"Długość sformatowanych danych: {name_surname_length}")
