2656899bfc2263b988724117c1179a952a27fbd4
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-06-05 15:15:12 +0200

    Update zad1.py

--- lesson2/zad1.py
+++ lesson2/zad1.py
@@ -1,9 +1,16 @@
 name = "Maksymilian"
-age = "20"
+age = 20
 grades_avg = 3.8
-student_status = 1
+student_status = True
 
-for i in (name, age, grades_avg, student_status):
-    
-    print(i, type(i))
-    
+print("Wartość zmiennej name:", name)
+print("Typ zmiennej name:", type(name))
+
+print("Wartość zmiennej age:", age)
+print("Typ zmiennej age:", type(age))
+
+print("Wartość zmiennej grades_avg:", grades_avg)
+print("Typ zmiennej grades_avg:", type(grades_avg))
+
+print("Wartość zmiennej student_status:", student_status)
+print("Typ zmiennej student_status:", type(student_status))
