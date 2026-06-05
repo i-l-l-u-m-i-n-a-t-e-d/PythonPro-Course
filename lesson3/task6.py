7e989fe28505b066cca51480be290ab5f71e5590
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-06-05 15:30:57 +0200

    Refactor text input handling and boolean check

--- lesson3/task6.py
+++ lesson3/task6.py
@@ -1,9 +1,10 @@
-text = bool(input("Wpisz dowolny ciąg znaków (tekst): "))
+text = input("Wpisz dowolny ciąg znaków: ")
 
-if text:
-    print("Tekst zawiera znaki (niepusty).")
+text_bool = bool(text)
 
+print(f"Wartość logiczna tekstu: {text_bool}")
+
+if text_bool:
+    print("Tekst zawiera znaki.")
 else:
     print("Nic nie wpisano.")
-    
-
