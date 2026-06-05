7e7b0cedf192a1aebcc2091c1314a908ca9241fc
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-06-05 15:35:51 +0200

    Update task8.py

--- lesson3/task8.py
+++ lesson3/task8.py
@@ -1,23 +1,23 @@
 def age_of_dog_conv(age):
     
-    r = 0
-    
-    for i in range(age):
+    if age == 1:
+        r = 15
         
-        if i == 0:
-            r += 15
+    elif age == 2:
+        r = 15 + 9
         
-        elif i == 1:
-            r += 9
-        
-        else:
-            r += 5
+    else:
+        r = 15 + 9 + (age - 2) * 5
     
     return r
 
 
 age = int(input("Podaj wiek psa (wpisz tylko cyfry): "))
 
-result = age_of_dog_conv(age)
+if age <= 0:
+    print("Wiek psa musi być większy od 0.")
+    
+else:
+    result = age_of_dog_conv(age)
 
-print(f"Wiek tego psa w ludzkich latach to: {result}")
+    print(f"Wiek tego psa w ludzkich latach to: {result}")
