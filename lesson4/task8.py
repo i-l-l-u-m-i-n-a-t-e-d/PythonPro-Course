1fa8a182b5fec79c50e317bd996242f5ef4e9796
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-06-05 15:45:58 +0200

    Update task8.py

--- lesson4/task8.py
+++ lesson4/task8.py
@@ -1,28 +1,16 @@
 imiona = ["Anna", "Jan", "Piotr", "Kasia"]
 
-name = input("Podaj imie do wyszukania ").strip().capitalize()
+name = input("Podaj imie do wyszukania: ").strip().capitalize()
 
 
-
-#for-else
 
 for i in imiona:
 
     if name == i:
 
         print("Znaleziono!")
-        not_found = False
         break
 
-
 else:
+
     print("Nie znaleziono imienia na liście.")
-
-
-
-
-
-
-
-
-
