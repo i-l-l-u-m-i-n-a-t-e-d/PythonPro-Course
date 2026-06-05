f2d8498445564c34236a17e66c5204bd462947ab
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-06-05 15:20:32 +0200

    Update zad9.py

--- lesson2/zad9.py
+++ lesson2/zad9.py
@@ -1,23 +1,8 @@
-p = int(input("Wprowadź 1 wartość logiczną (wpisz 0 lub 1, 0 - False, 1 - True): "))
-
-q = int(input("Wprowadź 2 wartość logiczną (wpisz 0 lub 1, 0 - False, 1 - True): "))
-
+p = bool(int(input("Wprowadź 1 wartość logiczną (0 - False, 1 - True): ")))
+q = bool(int(input("Wprowadź 2 wartość logiczną (0 - False, 1 - True): ")))
 
 and_op = p and q
-
 or_op = p or q
 
-if and_op:
-    print("1 - True")
-
-else:
-    print("0 - False")
-
-
-if or_op:
-    print("1 - True")
-
-else:
-    print("0 - False")
-
-
+print(f"Wynik AND: {and_op}")
+print(f"Wynik OR: {or_op}")
