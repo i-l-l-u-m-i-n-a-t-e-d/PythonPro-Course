de3db9ebaba89594b101fd4b11c42b3c885041d0
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-06-05 15:26:22 +0200

    Update task2.py

--- lesson3/task2.py
+++ lesson3/task2.py
@@ -1,17 +1,24 @@
-a=256
-b=256
-c=256
+a = int("256")
+b = int("256")
+c = int("256")
 
 print(f"Id zmiennej a: {id(a)}")
 print(f"Id zmiennej b: {id(b)}")
 print(f"Id zmiennej c: {id(c)}")
 
-d=257
-e=257
-f=257
+print(f"a is b: {a is b}")
+print(f"b is c: {b is c}")
+
+d = int("257")
+e = int("257")
+f = int("257")
 
 print(f"Id zmiennej d: {id(d)}")
 print(f"Id zmiennej e: {id(e)}")
 print(f"Id zmiennej f: {id(f)}")
 
-#ta sama wartosc zmiennych sprawia ze Python traktuje je tak samo i dlatego kazda trojka ma to samo id
+print(f"d is e: {d is e}")
+print(f"e is f: {e is f}")
+
+# dla 256 Python zwykle pokazuje ten sam id, bo małe liczby są trzymane w pamięci
+# przy 257 może być inaczej, bo ta liczba nie mieści się już w tym standardowym zakresie
