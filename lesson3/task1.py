5b38a6d00693a8110a53a4c63d1b62c77c5a2d74
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-06-05 15:24:12 +0200

    Update task1.py

--- lesson3/task1.py
+++ lesson3/task1.py
@@ -1,18 +1,24 @@
-a = float(input("Podaj pierwszą liczbę: ").replace(",","."))
-b = float(input("Podaj drugą liczbę: ").replace(",","."))
+a = float(input("Podaj pierwszą liczbę: ").replace(",", "."))
+b = float(input("Podaj drugą liczbę: ").replace(",", "."))
 
-print(f"Wynik dodawania: {a+b}")
-print(f"Wynik odejmowania: {a-b}")
+print(f"Pierwsza liczba: {a}")
+print(f"Typ pierwszej liczby: {type(a)}")
+print(f"Identyfikator pierwszej liczby: {id(a)}")
 
-multi = a*b
+print(f"Druga liczba: {b}")
+print(f"Typ drugiej liczby: {type(b)}")
+print(f"Identyfikator drugiej liczby: {id(b)}")
 
-print(f"Wynik mnozenia: {multi}")
+dodawanie = a + b
+odejmowanie = a - b
+mnozenie = a * b
+
+print(f"Wynik dodawania: {dodawanie}")
+print(f"Wynik odejmowania: {odejmowanie}")
+print(f"Wynik mnożenia: {mnozenie}")
 
 if b == 0:
-
     print("Nie można dzielić przez 0.")
-
 else:
-    
-    divide = a/b
-    print(f"Wynik dzielenia: {divide}")
+    dzielenie = a / b
+    print(f"Wynik dzielenia: {dzielenie}")
