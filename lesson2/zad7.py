60364541d9363cfed9c835e9f242cacdb344f4b9
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-06-05 15:18:32 +0200

    Update zad7.py

--- lesson2/zad7.py
+++ lesson2/zad7.py
@@ -1,5 +1,3 @@
 punkt = (10, 20, 30)
 
-punkt[0] = 15 #tuple to niemutowalana (nie da sie zmienic) kolekcja elementow
-
-
+punkt[0] = 15  # Błąd, ponieważ krotka tuple jest niemutowalna, czyli po utworzeniu nie można zmieniać jej elementów.
