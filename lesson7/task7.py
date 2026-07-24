44c775e3aa8ae11af268938384441a2367dfde82
i-l-l-u-m-i-n-a-t-e-d <maxbiz762@gmail.com>
2026-07-24 20:51:58 +0200

    Update task7.py
    Updated the function pobierz_wartosc to use .get() method and added a new version with try...except for error handling.

--- lesson7/task7.py
+++ lesson7/task7.py
@@ -1,40 +1,32 @@
-#version with .get()
-
-
-# def pobierz_wartosc(slownik, klucz):
-
-#     return slownik.get(klucz)
-
-# dict = {
-#     "imie": "Anna",
-#     "wiek": 28,
-#     "miejscowosc": "Warszawa",
-#     "status_studenta": False
-# }
-
-# r = pobierz_wartosc(dict, "")
-
-# print(r)
+# wersja 1 - z .get()
 
 def pobierz_wartosc(slownik, klucz):
+    return slownik.get(klucz)
 
-    try:
 
-        return slownik[klucz]
-
-    except KeyError:
-
-        return None
-
-  
-
-dict = {
+slownik = {
     "imie": "Anna",
     "wiek": 28,
     "miejscowosc": "Warszawa",
     "status_studenta": False
 }
 
-r = pobierz_wartosc(dict, "status_studenta")
+r = pobierz_wartosc(slownik, "status_studenta")
+
+print(r)
+
+
+# wersja 2 - z try...except KeyError
+
+def pobierz_wartosc_try(slownik, klucz):
+
+    try:
+        return slownik[klucz]
+
+    except KeyError:
+        return None
+
+
+r = pobierz_wartosc_try(slownik, "status_studenta")
 
 print(r)
